4362c4ad51103def8ff4e0e6f070119c893cbd80
Garrett Breach <breachg1992@gmail.com>
2020-10-16 16:42:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,6 +33,24 @@ var age = "13"
     case "13":
       console.log("You are a teenager'");
       break;
+      case "14":
+        console.log("You are a teenager'");
+        break;
+        case "15":
+          console.log("You are a teenager'");
+          break;
+          case "16":
+            console.log("You are a teenager'");
+            break;
+            case "17":
+              console.log("You are a teenager'");
+              break;
+              case "18":
+                console.log("You are a teenager'");
+                break;
+                case "19":
+                  console.log("You are a teenager'");
+      break;
     default:
       console.log('You have an age');
   }
