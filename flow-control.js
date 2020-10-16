8362b42b3a6e3a80a36b8a44baea93b35f2d598c
Garrett Breach <breachg1992@gmail.com>
2020-10-16 16:34:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,39 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!' }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!' }
+    else {
+      return 'You are not a teenager'
+    }
 }
 
 function ageChecker(age) {
-
-}
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!' }
+    else if (age <=12) {
+      return 'You are a kid' }
+      else {
+        return 'You are a grownup' }
+      }
 
 function ternaryTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager' }
 
-}
+      return 'You are not a teenager'
+    }
 
 function switchAge(age) {
-
+var age = "13,14,15,16,17,18,19"
+  switch(age) {
+    case "age >= 13 && age <= 19":
+      console.log("You are a teenager'");
+      break;
+    default:
+      console.log("You have an age");
+  }
 }
