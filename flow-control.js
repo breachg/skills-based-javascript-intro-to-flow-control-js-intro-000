ce3546cc5906e96cde3876f81512fa5dffcf41e2
Garrett Breach <breachg1992@gmail.com>
2020-10-16 16:47:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 var age = "13"
   switch(age) {
-    case "13":
+    case === 13:
       console.log('You are a teenager');
       break;
       case "14":
